d85f8dd5f62e4fcdec9acde07d4179c68b0b5e8c
Sarah-LB <slloving@gmail.com>
2022-12-12 23:24:09 +0000

    fix issues with env.py, reinstall flask and pymongo. Get everything back up and running

--- .env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "M[EumR@axY![$I*.mIcK}yj|Fdz]Q7")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://SarahDB:L0vingDatabase@myfirstcluster.vfvraid.mongodb.net/book_club?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "book_club")
